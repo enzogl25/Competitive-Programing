f85a8e972d5b018982d46c4c9af093468e75a295
enzogl <enzo.guerra@me.com>
2025-02-20 16:05:15 -0300

    add Palindrome number

--- LeetCode/Palindrome Number.cpp
+++ LeetCode/Palindrome Number.cpp
@@ -5,7 +5,7 @@ class Solution {
     public:
         bool isPalindrome(int x) {
             string s = to_string(x);
-            for(int i = 0; i < s.size()/2; i++){
+            for(int i = 0; i < (int)s.size()/2; i++){
                 if (s[i] != s[s.size()-i-1])return false;
             }
             return true;
