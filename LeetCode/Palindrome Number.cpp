d1682d51da8d10f0b589b5270fdddcd024b5b15f
enzogl <enzo.guerra@me.com>
2025-02-20 16:05:03 -0300

    add Palindrome number

---
+++ LeetCode/Palindrome Number.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        bool isPalindrome(int x) {
+            string s = to_string(x);
+            for(int i = 0; i < s.size()/2; i++){
+                if (s[i] != s[s.size()-i-1])return false;
+            }
+            return true;
+        }    
+    };
+
+    int main(){
+        Solution sol;
+
+        cout << sol.isPalindrome(121) << endl;
+    }
