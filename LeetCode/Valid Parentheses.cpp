f7383a02877feb9e37da62baf7a9de715da27365
enzogl <enzo.guerra@me.com>
2025-02-22 14:29:05 -0300

    add Valid Parentheses

---
+++ LeetCode/Valid Parentheses.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        bool isValid(string s) {
+            stack<char>  st;
+            for(char c : s){
+                if(c == '(' or c == '[' or c == '{'){
+                    st.push(c);
+                }
+                else{
+                    if(st.empty())return false;
+                    char top = st.top();
+                    if ((c == ')' && top != '(') || 
+                    (c == ']' && top != '[') || 
+                    (c == '}' && top != '{')) {
+                    return false;
+                    }
+                    st.pop();
+                }
+            }
+            
+            return st.empty();
+        }
+       
+    };
+
+
+
+int main(){
+    Solution sol;
+    cout << sol.isValid("]") << endl;
+}
