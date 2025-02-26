b658dd55fe5e280a0609e328a7e7e47a7585c9a5
enzogl <enzo.guerra@me.com>
2025-02-26 15:46:55 -0300

    add 1052

---
+++ Beecrowd/1052.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define f first
+#define s second
+#define pb push_back
+#define ppb pop_back
+using ll = long long;
+
+int main() {
+    _
+    vector<string> v = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    int n; cin >> n;
+    cout << v[n-1] << endl;   
+    return 0;
+}
