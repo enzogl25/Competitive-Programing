101690e446fec0a3641333e4fc08f4fb6a0ba57d
enzogl <enzo.guerra@me.com>
2025-03-08 18:18:26 -0300

    add 1067

---
+++ Beecrowd/1067.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define f first
+#define s second
+#define pb push_back
+#define ppb pop_back
+using ll = long long;
+
+int main() {
+    _
+    int n; cin >> n;
+    for(int i = 1; i <= n; i+=2)cout << i << endl;   
+    return 0;
+}
