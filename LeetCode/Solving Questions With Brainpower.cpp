38c00424aacbcca2d344731f862a307140fb4abb
enzogl <enzo.guerra@me.com>
2025-04-01 11:39:59 -0300

    add solving quenstions with brainpower

---
+++ LeetCode/Solving Questions With Brainpower.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        long long mostPoints(vector<vector<int>>& questions) {
+            int size = questions.size();
+            vector<long long> dp(size + 1,0);
+            for(int i = size-1; i >= 0; i--){
+                long long solve = questions[i][0];
+                int nextIndex = i + questions[i][1] + 1;
+                if(nextIndex < size){
+                    solve += dp[nextIndex];
+                    dp[i] = max(solve, dp[i+1]);
+                }
+                return dp[0];
+            }
+                
+            
+            
+            
+            
+        }
+    };
